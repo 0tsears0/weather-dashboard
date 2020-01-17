943d4505d120a2f9f27c542a7dd32458da93a272
Thomas Sears <0tsears0@gmail.com>
2020-01-16 19:09:40 -0500

    added pseudocode for variables needed for request

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 // 30d713a82ab035f6afc1e90d950c8e49
 
 let apiKey = "30d713a82ab035f6afc1e90d950c8e49";
-let URL =`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`
+let URL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`
+//current date
+
+// Temp (main, temp), humidity (main, humidity), windspeed (wind, speed), uv index ()
+
+// 5 day forecast
+
+
 function getWeather() {
   let city = $("#search-input").val()
   console.log(city)
