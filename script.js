8e2adac5d472219132e1a264fbb033921cdd4dff
Thomas Sears <0tsears0@gmail.com>
2020-01-16 19:04:13 -0500

    added URL for the search

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // 30d713a82ab035f6afc1e90d950c8e49
 
+let apiKey = "30d713a82ab035f6afc1e90d950c8e49";
+let URL =`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`
 function getWeather() {
   let city = $("#search-input").val()
   console.log(city)
